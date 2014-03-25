fae0fcb3f81568413b18ce758f34edf7c2f6d480
Steve Long <steven989@gmail.com>
2014-03-25 13:53:06 -0400

    finished the name and age thingy

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,13 @@
 puts "What is your name"
+
+name = gets.chomp
+
+puts "Hi #{name}! How old are you?"
+
+age = gets.chomp.to_i
+
+birthyear = Time.new.year.to_i - age - 1
+
+puts "So you were born in #{birthyear}"
+
+
